01288a338f6e9ca27e8eb4a3525ae46034aedba9
Rolf Christian Jørgensen <114920418+rcj-siteimprove@users.noreply.github.com>
2024-08-19 16:09:11 +0200

    Add Alfa and upload

--- tests/accessibility.spec.ts
+++ tests/accessibility.spec.ts
@@ -1,12 +1,36 @@
 import { test, expect } from "@playwright/test";
 import { Playwright } from "@siteimprove/alfa-playwright";
+import { Audit, SIP, Logging } from "@siteimprove/alfa-test-utils";
 
-test("can get alfa page", async ({ page }) => {
+test("page is accessible", async ({ page }) => {
   await page.goto("http://localhost:8080/");
 
   const document = await page.evaluateHandle(() => window.document);
 
   const alfaPage = await Playwright.toPage(document);
 
-  expect(alfaPage).toBeTruthy();
+  const alfaResult = await Audit.run(alfaPage);
+
+  if (
+    process.env.SI_USER_NAME !== undefined &&
+    process.env.SI_API_KEY !== undefined
+  ) {
+    const pageReportURL = await SIP.upload(alfaResult, {
+      userName: process.env.SI_USER_NAME!, // email address of the user.
+      apiKey: process.env.SI_API_KEY!, // API key generated in the platform.
+    });
+
+    Logging.result(alfaResult, pageReportURL);
+  } else {
+    Logging.result(alfaResult);
+  }
+
+  const failingRules = alfaResult.resultAggregates.filter(
+    (aggregate) => aggregate.failed > 0,
+  );
+
+  expect(
+    failingRules.size,
+    `The page has ${failingRules.size} failing rules`,
+  ).toBe(0);
 });
