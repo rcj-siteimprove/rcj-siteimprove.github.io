2bf17e3559a42a0d29f8816a4d65eeef210f7011
Rolf Christian Jørgensen <114920418+rcj-siteimprove@users.noreply.github.com>
2024-12-03 12:36:35 +0100

    Add mask-composite field

--- css-properties/mask/main.js
+++ css-properties/mask/main.js
@@ -1,47 +1,60 @@
 const params = new URL(document.location.href).searchParams;
+
 const tree = document.getElementById("tree");
 const ornament = document.getElementById("ornament");
 const gingerbread = document.getElementById("gingerbread");
 const heart = document.getElementById("heart");
+
 const repeat = document.getElementById("repeat");
 const clip = document.getElementById("clip");
+const composite = document.getElementById("composite");
+
 const masked = document.getElementById("masked");
 const computed = document.getElementById("computed");
+
 tree.checked = params.get("tree") !== null;
 ornament.checked = params.get("ornament") !== null;
 gingerbread.checked = params.get("gingerbread") !== null;
 heart.checked = params.get("heart") !== null;
+
 repeat.value = params.get("repeat");
 clip.value = params.get("clip");
+composite.value = params.get("composite");
+
 masked.style.maskRepeat = repeat.value;
 masked.style.maskClip = clip.value;
+masked.style.maskComposite = composite.value;
+
 const images = [];
 if (tree.checked) {
-    images.push("url(tree.svg)");
+  images.push("url(tree.svg)");
 }
 if (ornament.checked) {
-    images.push("url(ornament.svg)");
+  images.push("url(ornament.svg)");
 }
 if (gingerbread.checked) {
-    images.push("url(gingerbread.svg)");
+  images.push("url(gingerbread.svg)");
 }
 if (heart.checked) {
-    images.push("url(heart.svg)");
+  images.push("url(heart.svg)");
 }
 if (images.length > 0) {
-    masked.style.maskImage = images.join(", ");
+  masked.style.maskImage = images.join(", ");
 }
+
 addRow("mask-image");
 addRow("mask-clip");
 addRow("mask-repeat");
+addRow("mask-composite");
+
 function addRow(name) {
-    const row = computed.insertRow();
-    row.insertCell(0).append(name);
-    row
-        .insertCell(1)
-        .append(window.getComputedStyle(masked).getPropertyValue(name));
+  const row = computed.insertRow();
+  row.insertCell(0).append(name);
+  row
+    .insertCell(1)
+    .append(window.getComputedStyle(masked).getPropertyValue(name));
 }
+
 fetch("https://baconipsum.com/api/?type=meat-and-filler&paras=18&format=text")
-    .then((resp) => resp.text())
-    .then((text) => masked.append(text));
-export {};
+  .then((resp) => resp.text())
+  .then((text) => masked.append(text));
